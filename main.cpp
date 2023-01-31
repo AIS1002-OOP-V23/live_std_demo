202711c249d11b1058e287ca49ce7865ed309270
Lars Ivar Hatledal <laht@ntnu.no>
2023-01-31 13:37:36 +0100

    reset master

--- main.cpp
+++ main.cpp
@@ -1,30 +1,7 @@
 #include <iostream>
-#include <optional>
-
-// returns -1 if fail
-std::optional<int> mayFail(int n) {
-
-    if (n % 2 == 0) {
-        // fail
-        return std::nullopt;
-    } else {
-        // success
-        return 42;
-    }
-}
 
 int main() {
-
-    for (int i = 0; i < 10; i++) {
-        auto result = mayFail(i);
-        if (!result) {
-            // it failed
-            std::cout << "fail: " << std::endl;
-        } else {
-            // cure cancer
-            std::cout << "yay! value=" << result.value() << std::endl;
-        }
-    }
+    
 
     return 0;
 }
